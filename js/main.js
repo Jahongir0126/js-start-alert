6c78e26da322cf4ad5886b43b225f17e79589df8
Mansur <tursunboyevjahongir04@yandex.ru>
2022-08-16 10:53:14 +0500

    initial

---
+++ js/main.js
@@ -0,0 +1,29 @@
+// console.log ("Helo world");
+
+// var firstname = "Jahongir";
+
+// var year = 2000;
+// var age = 0;
+// age = year + 2;
+// console.log(age);
+
+// var sd23 =   `Helo world`;
+// console.log(sd23);
+var obj_savol = {}
+obj_savol.savol1 = prompt("Ismingizni kiriting :", "Shohruhbek")
+obj_savol.savol2 = prompt("Familiyangizni kiriting :", "Murodullayev")
+obj_savol.savol3 = prompt("yoshingizni kiriting :", "21")
+obj_savol.savol4 = prompt(" yoqtirgan filmingiz :", "The Avengers")
+obj_savol.savol5 = prompt("O'qish joyi:", "TUIT")
+obj_savol.savol6 = prompt("Kursingiz:", "3")
+obj_savol.savol7 = prompt("Fakultet:", "KIF")
+obj_savol.savol8 = prompt("Hobbi:", "Dasturlash")
+obj_savol.savol9 = prompt("Maoshingiz:", "Secret")
+obj_savol.savol10 = prompt("Qondaysiz:", "Zo'r")
+
+var yosh = 2022 - obj_savol.savol3;
+alert(` Ismingiz : ${obj_savol.savol1} \n Familiyangiz : ${obj_savol.savol2} 
+ Yilingiz : ${yosh} \n Film : ${obj_savol.savol4} 
+ Universitet : ${obj_savol.savol5} \n Kursingiz : ${obj_savol.savol6} 
+ Yo'nalish : ${obj_savol.savol7} \n Hobbiyingiz : ${obj_savol.savol8}
+ Maoshingiz : ${obj_savol.savol9} \n Ahvolingiz : ${obj_savol.savol10} `)
